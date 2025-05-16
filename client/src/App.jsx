1f66e56ed640781745ddfa68aff70aa0dd51ee1e
SubeeshCk <subeeshck.dev@gmail.com>
2025-05-16 11:14:08 +0530

    Header added with links to various pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "../src/pages/About"
 import SignIn from "../src/pages/SignIn"
 import SignUp from "../src/pages/SignUp"
 import Profile from "../src/pages/Profile"
+import Header from "./components/Header"
 
 const App = () => {
   return (
       <BrowserRouter>
+          <Header/>
           <Routes>
              <Route path="/" element = {<Home/>}></Route>
              <Route path="/about" element = {<About/>}></Route>
