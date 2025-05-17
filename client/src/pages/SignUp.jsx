c5093a10bfd5fb20b575ff014f4dfb30da6eda23
SubeeshCk <subeeshck.dev@gmail.com>
2025-05-17 16:02:24 +0530

    signin funcionality completed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -27,6 +28,7 @@ const SignUp = () => {
         setError(true);
         return;
       }
+      navigate('/sign-in')
     } catch (error) {
       setLoading(false);
       setError(true);
@@ -65,7 +67,7 @@ const SignUp = () => {
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to="/sign-in">
-          <span className="text-blue-500">Sign in</span>
+          <span className="text-blue-500">Sign In</span>
         </Link>
       </div>
       <p className="text-red-600 mt-5">{ error && 'Something went wrong'}</p>
