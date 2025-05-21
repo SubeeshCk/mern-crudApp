bc272c48804920258386f0203b39391e9f903a98
SubeeshCk <subeeshck.dev@gmail.com>
2025-05-21 12:40:47 +0530

    updated profile as private and header with user image.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "../src/pages/Home"
-import About from "../src/pages/About"
-import SignIn from "../src/pages/SignIn"
-import SignUp from "../src/pages/SignUp"
-import Profile from "../src/pages/Profile"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import SignIn from "../src/pages/SignIn";
+import SignUp from "../src/pages/SignUp";
+import Profile from "../src/pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
-      <BrowserRouter>
-          <Header/>
-          <Routes>
-             <Route path="/" element = {<Home/>}></Route>
-             <Route path="/about" element = {<About/>}></Route>
-             <Route path="/sign-in" element = {<SignIn/>}></Route>
-             <Route path="/sign-up" element = {<SignUp/>}></Route>
-             <Route path="/profile" element = {<Profile/>}></Route>
-          </Routes>
-      </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
